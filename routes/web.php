d5a448bd035cf559d4e12e7b54eae04285c1fd5c
jonathan-martz <github@jmartz.de>
2019-12-26 16:09:07 +0100

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/user/register', [
     'middleware' => ['xss', 'https'],
-    'uses' => 'App\Http\Controllers\LoginController@user'
+    'uses' => 'App\Http\Controllers\RegisterController@user'
 ]);
