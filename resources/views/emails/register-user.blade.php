fab81e8a52c69c2707e33ed8d65365d45166d426
jonathan-martz <github@jmartz.de>
2019-12-28 15:36:46 +0100

    update template

--- resources/views/emails/register-user.blade.php
+++ resources/views/emails/register-user.blade.php
@@ -2,7 +2,7 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Register User Activation Email - hetzner.jmartz.de</title>
+    <title>Register User Activation Email - hetzner-dev.jmartz.de</title>
     <link rel="stylesheet" type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
@@ -12,12 +12,12 @@
     <p><strong>Register User Activation Email</strong></p>
     <p>
         You create a new Account with Username {{ $username }} on <a
-                href="https://hetzner.jmartz.de/">hetzner.jmartz.de</a>.<br><br>
+                href="https://hetzner-dev.jmartz.de/">hetzner-dev.jmartz.de</a>.<br><br>
 
         If you doesnt create an Account on this Website, you can securly ignore this email.<br>
-        If no you can Click to <a href="https://hetzner.jmartz.de/user/activate?token={{$token}}">hetzner.jmartz.de</a>
+        If no you can Click to <a href="https://hetzner-dev.jmartz.de/user/activate?token={{$token}}">activate</a>
         <br>
-        your email Account of {{ $email }} on <a href="https://hetzner.jmartz.de/">hetzner.jmartz.de</a>.<br>
+        your email Account of {{ $email }} on <a href="https://hetzner-dev.jmartz.de/">hetzner-dev.jmartz.de</a>.<br>
     </p>
 </main>
 @component('general::footer') @endcomponent
