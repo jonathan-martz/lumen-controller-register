56e7ad647f6818f57e1746a83312c914674867f2
jonathan-martz <github@jmartz.de>
2019-12-29 20:59:02 +0100

    fix base url problem

--- resources/views/emails/register-user.blade.php
+++ resources/views/emails/register-user.blade.php
@@ -6,19 +6,22 @@
     <link rel="stylesheet" type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
-<!--| TODO fix urls depend on base url |-->
 <body>
 @component('general::header') @endcomponent
+@php
+    $baseurl = str_replace(['api-', 'https://'],'', URL::to('/'));
+    $baselink = str_replace(['api-'],'', URL::to('/'))
+@endphp
 <main>
     <p><strong>Register User Activation Email</strong></p>
     <p>
         You create a new Account with Username {{ $username }} on <a
-                href="https://hetzner-dev.jmartz.de/">hetzner-dev.jmartz.de</a>.<br><br>
+                href="{{ $baseurl  }}">{{ $baseurl  }}</a>.<br><br>
 
         If you doesnt create an Account on this Website, you can securly ignore this email.<br>
-        If no you can Click to <a href="https://hetzner-dev.jmartz.de/user/activate?token={{$token}}">activate</a>
+        If no you can Click to <a href="{{ $baselink  }}/user/activate?token={{$token}}">activate</a>
         <br>
-        your email Account of {{ $email }} on <a href="https://hetzner-dev.jmartz.de/">hetzner-dev.jmartz.de</a>.<br>
+        your email Account of {{ $email }} on <a href="{{ $baselink  }}">{{ $baseurl  }}</a>.<br>
     </p>
 </main>
 @component('general::footer') @endcomponent
